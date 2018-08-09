656909481677d643766b1fd6091a1b91cde0c1cf
shifali <shifali.gupta83@gmail.com>
2018-08-09 23:27:40 +0530

    seeding

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users =[];
+        $faker = Factory::create();
+        for($i=0; $i<10; $i++){
+            $date = date('y-m-d h:i:s', strtotime("2018-08-09 00:00:00 +{$i} days"));
+            $users[] = [
+                name=> $faker->name,
+                email=> $faker->email,
+                password=> bcrypt(12345),
+                created_at=> $date,
+                updated_at=> $date
+            ];
+        }
+        DB::table('users')->insert($users);
     }
 }
